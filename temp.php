43396714bb13be390a56af963b51a1475ac8aa93
koush912 <koushkrish2@gmail.com>
2024-07-02 01:50:29 +0530

    Update temp.php

--- temp.php
+++ temp.php
@@ -1,45 +1,24 @@
 <?php
 
-class HomeController
-{
-    public function index()
-    {
-        return $this->view();
-    }
-
-    public function convertTemperature($form)
-    {
-        if (isset($form['celsius'])) {
-            try {
-                $celsius = floatval($form['celsius']);
-                $fahrenheit = $celsius * 9 / 5 + 32;
-                $data['Celsius'] = $celsius;
-                $data['Fahrenheit'] = $fahrenheit;
-                return $this->view('Result', $data);
-            } catch (Exception $e) {
-                $data['ErrorMessage'] = 'Invalid input';
-                return $this->view('Error', $data);
-            }
-        } elseif (isset($form['fahrenheit'])) {
-            try {
-                $fahrenheit = floatval($form['fahrenheit']);
-                $celsius = ($fahrenheit - 32) * 5 / 9;
-                $data['Celsius'] = $celsius;
-                $data['Fahrenheit'] = $fahrenheit;
-                return $this->view('Result', $data);
-            } catch (Exception $e) {
-                $data['ErrorMessage'] = 'Invalid input';
-                return $this->view('Error', $data);
-            }
-        } else {
-            $data['ErrorMessage'] = 'No input provided';
-            return $this->view('Error', $data);
-        }
-    }
-
-    private function view($viewName = '', $data = [])
-    {
-        // Implement your view rendering logic here
-        // Return the rendered view
-    }
+// Function to convert Celsius to Fahrenheit
+function celsiusToFahrenheit($celsius) {
+    $fahrenheit = ($celsius * 9/5) + 32;
+    return $fahrenheit;
 }
+
+// Function to convert Fahrenheit to Celsius
+function fahrenheitToCelsius($fahrenheit) {
+    $celsius = ($fahrenheit - 32) * 5/9;
+    return $celsius;
+}
+
+// Test the functions
+$celsius = 25;
+$fahrenheit = celsiusToFahrenheit($celsius);
+echo "$celsius degrees Celsius is equal to $fahrenheit degrees Fahrenheit.\n";
+
+$fahrenheit = 77;
+$celsius = fahrenheitToCelsius($fahrenheit);
+echo "$fahrenheit degrees Fahrenheit is equal to $celsius degrees Celsius.\n";
+
+?>
